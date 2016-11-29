5894693b0f5e52343f75bcd8c7fbe38cbf263224
ksosina <mail@ksosina.com>
2016-11-28 20:42:51 -0500

    #KS; Cosmetic edits

--- calfdr.R
+++ calfdr.R
@@ -19,12 +19,12 @@ calFDR <- function(df){
     #rounded or not
     rr = rep(0,length(tt)) 
     #for those that are not truncated check if their values equal after rounding
-    rr[tt == 0] = (as.numeric(x[tt==0,1]) == round(as.numeric(x[tt==0,1]),2)) 
+    rr[tt == 0] = (as.numeric(x[tt == 0,1]) == round(as.numeric(x[tt == 0,1]),2)) 
     
     #pvalues
     pp = as.numeric(x[,1])
     out = calculateSwfdr(pp,tt,rr)
-    c("swfdr" = out$$pi0, unique(row.names(x)), unique(x[,4]))
+    c("swfdr" = out$pi0, unique(row.names(x)), unique(x[,4]))
   })
   
   plot.dat <- data.frame("swfdr" = result[1,], "journal" = result[2,],  "year" = result[3,])
