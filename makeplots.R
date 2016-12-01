1ea6df11f790baf097e188f4c376793e601f1b48
28pro92 <28pkundu1992@gmail.com>
2016-12-01 17:07:20 -0500

    Changed the class of year from integer to factor #PK

--- makeplots.R
+++ makeplots.R
@@ -2,6 +2,7 @@
 
 makeplot <- function(d)
 {
+  d$year <- as.factor(d$year)
   p <- ggplot(data = d, aes(x = year, y = swfdr, colour = journal, group = journal))
   p + geom_point() + geom_line()
 }
