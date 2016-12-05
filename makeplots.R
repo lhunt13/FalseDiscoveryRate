4f207fc7d1975f23dfc5076da20af7a947c192a3
28pro92 <28pkundu1992@gmail.com>
2016-12-05 15:22:00 -0500

    changed the sizes of axes #PK

--- makeplots.R
+++ makeplots.R
@@ -4,6 +4,6 @@ makeplot <- function(d)
 {
   d$year <- as.factor(d$year)
   p <- ggplot(data = d, aes(x = year, y = swfdr, colour = journal, group = journal))
-  p + geom_point() + geom_line()
+  p + geom_point() + geom_line() + xlab("Year") + ylab("False Discovery Rate") + theme(axis.text=element_text(size=12), axis.title=element_text(size=14,face="bold"))
 }
 
